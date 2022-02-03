3ca17299d045c92e995dcbb912e6dba566d758b1
Sebastian Plaza <sebastianplaza@gmail.com>
2022-02-03 22:26:29 +0100

    feat: add country api call

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,25 @@
 extern crate skim;
-use base64::encode;
 use reqwest::Client;
+use serde::{Deserialize, Serialize};
+use serde_json::{Result, Value};
 use skim::prelude::*;
-use std::collections::HashMap;
 use std::io::Cursor;
 
-async fn call_api(&self, path: String, json_map: HashMap<&str, String>) -> (u16, String) {
+async fn get_countries() -> Result<String> {
     let url = String::from("http://all.api.radio-browser.info/json/countries");
+
+    let (status, data) = call_api(url).await;
+
+    let countries: Vec<Country> = serde_json::from_str(&data)?;
+    let mut str: String = String::from("");
+    for country in countries {
+        let countryln = format!("{}\n", &country.name);
+        str.push_str(&countryln);
+    }
+    Ok(str)
+}
+
+async fn call_api(url: String) -> (u16, String) {
     let client = Client::new();
     let res = client
         .get(url)
@@ -17,26 +30,37 @@ async fn call_api(&self, path: String, json_map: HashMap<&str, String>) -> (u16,
     let data = res.text().await.expect("Problem extracting data");
     return (status, data);
 }
-pub fn main() {
+#[derive(Serialize, Deserialize)]
+struct Country {
+    name: String,
+    stationcount: u64,
+}
+
+fn skim_show(list: String) -> String {
     let options = SkimOptionsBuilder::default()
         .height(Some("50%"))
         .multi(false)
         .build()
         .unwrap();
 
-    let input = "aaaaa\nbbbb\nccc".to_string();
-
     // `SkimItemReader` is a helper to turn any `BufRead` into a stream of `SkimItem`
     // `SkimItem` was implemented for `AsRef<str>` by default
     let item_reader = SkimItemReader::default();
-    let items = item_reader.of_bufread(Cursor::new(input));
+    let items = item_reader.of_bufread(Cursor::new(list));
 
     // `run_with` would read and show items from the stream
     let selected_items = Skim::run_with(&options, Some(items))
         .map(|out| out.selected_items)
         .unwrap_or_else(|| Vec::new());
 
-    for item in selected_items.iter() {
-        print!("{}{}", item.output(), "\n");
-    }
+    selected_items[0].output().into_owned()
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let countries = get_countries().await?;
+    let country = skim_show(countries);
+    println!("selected country: {}", country);
+
+    Ok(())
 }
